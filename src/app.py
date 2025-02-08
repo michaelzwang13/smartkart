143867190e72c5e44d3c526450af60bc7869ce79
michaelzwang13 <mzw2010@nyu.edu>
2025-02-08 13:12:33 -0500

    new sql queries

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 #Import Flask Library
 from flask import Flask, render_template, request, session, url_for, redirect
 import pymysql.cursors
+import hashlib
 
 #Initialize the app from Flask
 app = Flask(__name__)
@@ -24,64 +25,76 @@ def hello():
 def login():
 	return render_template('login.html')
 
-#Define route for register
-@app.route('/register')
-def register():
-	return render_template('register.html')
-
 #Authenticates the login
 @app.route('/loginAuth', methods=['GET', 'POST'])
 def loginAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+    #grabs information from the forms
+    user_ID = request.form['user_ID']
+    password = request.form['password']
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = 'SELECT * FROM user WHERE username = %s and password = %s'
-	cursor.execute(query, (username, password))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	cursor.close()
-	error = None
-	if(data):
-		#creates a session for the the user
-		#session is a built in
-		session['username'] = username
-		return redirect(url_for('home'))
-	else:
-		#returns an error message to the html page
-		error = 'Invalid login or username'
-		return render_template('login.html', error=error)
-
-#Authenticates the register
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM user_account WHERE user_ID = %s'
+    cursor.execute(query, (user_ID))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    cursor.close()
+    error = None
+    if(data):
+        #if bcrypt.checkpw(password.encode('utf-8'), data['password'].encode('utf-8')):
+        if hash_password_md5(password)==data['password']:
+        #creates a session for the the user
+        #session is a built in
+            session['user_ID'] = user_ID
+            return redirect(url_for('user_home'))
+        else:
+            error = 'Invalid login or email_address'
+            return render_template('login.html', error=error)
+            
+    else:
+           #returns an error message to the html page
+        error = 'Invalid login or email_address'
+        return render_template('customer_login.html', error=error)
+      
 @app.route('/registerAuth', methods=['GET', 'POST'])
 def registerAuth():
-	#grabs information from the forms
-	username = request.form['username']
-	password = request.form['password']
+    #grabs information from the forms
+    user_ID = request.form['user_ID']
+    password = request.form['password']
+    email_address = request.form['email_address']
 
-	#cursor used to send queries
-	cursor = conn.cursor()
-	#executes query
-	query = 'SELECT * FROM user WHERE username = %s'
-	cursor.execute(query, (username))
-	#stores the results in a variable
-	data = cursor.fetchone()
-	#use fetchall() if you are expecting more than 1 data row
-	error = None
-	if(data):
-		#If the previous query returns data, then user exists
-		error = "This user already exists"
-		return render_template('register.html', error = error)
-	else:
-		ins = 'INSERT INTO user VALUES(%s, %s)'
-		cursor.execute(ins, (username, password))
-		conn.commit()
-		cursor.close()
-		return render_template('index.html')
+    hashed_password = hash_password_md5(password)#bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    for key, value in request.form.items():
+        if value !='':
+            if 'phoneNumbers' in key:  # Handle array-style inputs for phone numbers
+                phone_number.append(value)
+    #cursor used to send queries
+    cursor = conn.cursor()
+    #executes query
+    query = 'SELECT * FROM customer WHERE email_address = %s'
+    cursor.execute(query, (email_address))
+    #stores the results in a variable
+    data = cursor.fetchone()
+    #use fetchall() if you are expecting more than 1 data row
+    error = None
+    if(data):
+        #If the previous query returns data, then user exists
+        error = "This email already exists"
+        return render_template('customer_register.html', error = error)
+    else:
+        ins = 'INSERT INTO customer VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        cursor.execute(ins, (email_address, hashed_password, first_name, last_name, building_name,
+                            street_name, apt_num, city, state, zipcode, date_of_birth,
+                            passport_number, passport_expiration, passport_country))
+        ins_phone = 'INSERT INTO customer_phone_number VALUES(%s, %s)'
+        for num in phone_number:
+            cursor.execute(ins_phone, (email_address,num))
+        conn.commit()
+        cursor.close()
+        session['email_address'] = email_address
+        return render_template('customer_home.html',section='search-flights',email_address = email_address)
 
 
 @app.route('/shopping_trip', methods=['GET'])
@@ -124,6 +137,13 @@ def add_item():
 def logout():
 	session.pop('username')
 	return redirect('/')
+
+def hash_password_md5(password):
+    # Create an MD5 hash object
+    md5 = hashlib.md5()
+    # Encode and hash the password
+    md5.update(password.encode('utf-8'))
+    return md5.hexdigest()
 		
 app.secret_key = 'some key that you will never guess'
 #Run the app on localhost port 5000
--- src/database/inserts.sql
+++ src/database/inserts.sql
@@ -1,42 +1,48 @@
--- Basic functionality
+-- Select the database
+USE hacknyu25;
 
-CREATE TABLE user(
+-- Create the user table (renamed to user_account)
+CREATE TABLE user_account (
     user_ID VARCHAR(50),
     email VARCHAR(50) NOT NULL,
     password VARCHAR(50) NOT NULL,
     date_registered DATETIME NOT NULL,
-    PRIMARY KEY(user_ID)
-)
+    PRIMARY KEY (user_ID)
+);
 
-CREATE TABLE cart(
-    cart_ID INT,
+-- Create the cart table
+CREATE TABLE cart (
+    cart_ID INT AUTO_INCREMENT,
     user_ID VARCHAR(50) NOT NULL,
     date_brought DATETIME NOT NULL,
-    PRIMARY KEY(cart_ID)
-    FOREIGN KEY(user_ID) AND REFERENCES user(user_ID)
-)
+    PRIMARY KEY (cart_ID),
+    FOREIGN KEY (user_ID) REFERENCES user_account(user_ID)
+);
 
-CREATE TABLE item(
-    item_ID INT,
+-- Create the item table
+CREATE TABLE item (
+    item_ID INT AUTO_INCREMENT,
     cart_ID INT NOT NULL,
     quantity INT NOT NULL,
-    PRIMARY KEY(item_ID)
-    FOREIGN KEY(cart_ID) AND REFERENCES cart(cart_ID)
-)
+    PRIMARY KEY (item_ID),
+    FOREIGN KEY (cart_ID) REFERENCES cart(cart_ID)
+);
 
--- Achievement functionality
-
-CREATE TABLE achievements(
-    achievement_ID INT,
+-- Create the achievements table
+CREATE TABLE achievements (
+    achievement_ID INT AUTO_INCREMENT,
     name VARCHAR(100) NOT NULL,
     points INT NOT NULL,
     description VARCHAR(500) NOT NULL,
-)
+    PRIMARY KEY (achievement_ID)
+);
 
-CREATE TABLE user_achievements(
-    user_ID INT,
+-- Create the user_achievements table
+CREATE TABLE user_achievements (
+    user_ID VARCHAR(50) NOT NULL,
     achievement_ID INT NOT NULL,
     date_earned DATETIME NOT NULL,
-    FOREIGN KEY(user_ID) AND REFERENCES user(user_ID)
-    FOREIGN KEY(achievement_ID) AND REFERENCES achievement(achievement_ID)
-)
+    PRIMARY KEY (user_ID, achievement_ID),
+    FOREIGN KEY (user_ID) REFERENCES user_account(user_ID),
+    FOREIGN KEY (achievement_ID) REFERENCES achievements(achievement_ID)
+);
