a824f599d69fe2c735ec73254a33126cb5c0d867
Michael Wang <mzw2010@nyu.edu>
2025-08-15 13:55:59 -0700

    fixed nutrition goals not updating

--- app.py
+++ app.py
@@ -7,17 +7,3 @@ if __name__ == '__main__':
     # The host must be set to '0.0.0.0' to be accessible from outside the container
     # if you are using Docker
     app.run(host='127.0.0.1', port=5001, debug=True)
-    
-    
-    from openai import OpenAI
-client = OpenAI()
-
-models = ["gpt-5", "gpt-5-mini", "gpt-5-nano"]
-
-for model in models:
-  response = client.responses.create(
-      model=model,
-      input="Write a one-sentence bedtime story about a unicorn."
-  )
-
-  print(response.output_text)
